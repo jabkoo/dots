5b901884075a2ed99a1130cc47d131e7481dff0c
jabkoo <jabrzyk.k@gmail.com>
2026-02-07 13:54:44 +0100

    Remove nvim config

--- _scripts/steps/stow.sh
+++ _scripts/steps/stow.sh
@@ -7,7 +7,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 stow_packages() {
 	title "Stowing packages"
 
-	stow -d $DOTFILES -t $HOME git ssh zsh ghostty nvim mise
+	stow -d $DOTFILES -t $HOME git ssh zsh ghostty mise
 }
 
 if stow_packages; then
